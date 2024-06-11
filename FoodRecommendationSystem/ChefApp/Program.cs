17e136d37b32e13a01fad67e9e9c0cfd9accacf5
Bhawana Agrawal <bhawana.agrawal@intimetec.com>
2024-06-11 16:05:26 +0530

    Created the project structure to be used

---
+++ FoodRecommendationSystem/ChefApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Chef App started...");
+        while (true)
+        {
+            Console.Write("Enter notification message: ");
+            string message = Console.ReadLine();
+
+            SendNotification(message);
+        }
+    }
+
+    static void SendNotification(string message)
+    {
+        TcpClient client = new TcpClient("127.0.0.1", 9000);
+        NetworkStream stream = client.GetStream();
+
+        byte[] buffer = Encoding.ASCII.GetBytes(message);
+        stream.Write(buffer, 0, buffer.Length);
+
+        buffer = new byte[1024];
+        int bytesRead = stream.Read(buffer, 0, buffer.Length);
+        string response = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+        Console.WriteLine(response);
+
+        client.Close();
+    }
+}
---
+++ FoodRecommendationSystem/EmployeeApp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Program
+{
+    private static TcpListener _listener;
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Employee App started...");
+
+        _listener = new TcpListener(IPAddress.Any, 9000);
+        _listener.Start();
+        Console.WriteLine("Notification Server started...");
+
+        while (true)
+        {
+            TcpClient client = _listener.AcceptTcpClient();
+            Thread thread = new Thread(HandleClient);
+            thread.Start(client);
+        }
+    }
+
+    private static void HandleClient(object obj)
+    {
+        TcpClient client = (TcpClient)obj;
+        NetworkStream stream = client.GetStream();
+        byte[] buffer = new byte[1024];
+        int bytesRead = stream.Read(buffer, 0, buffer.Length);
+        string message = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+
+        Console.WriteLine($"Notification received: {message}");
+
+        byte[] response = Encoding.ASCII.GetBytes("Notification sent to employees");
+        stream.Write(response, 0, response.Length);
+
+        client.Close();
+    }
+}
+
