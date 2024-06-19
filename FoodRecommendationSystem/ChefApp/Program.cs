bd991dfbce4f6aa931fc53477c93489146504f4e
Bhawana Agrawal <bhawana.agrawal@intimetec.com>
2024-06-19 17:48:09 +0530

    Added sending notification functionality

--- FoodRecommendationSystem/ChefApp/Program.cs
+++ FoodRecommendationSystem/ChefApp/Program.cs
@@ -15,12 +15,14 @@ class Program
         Console.WriteLine("Chef Console");
         Console.WriteLine("1. View Menu");
         Console.WriteLine("2. Get Recommended Dishes");
+        Console.WriteLine("3. Send Notification");
         Console.WriteLine("5. To Exit");
 
         var menuActions = new Dictionary<string, Action>
         {
             { "1", () => ViewMenu(client) },
             { "2", () => SelectMealOptions(client) },
+            { "3", () => SendNotification(client) },
             { "5", () => ExitProgram() }
         };
 
@@ -43,6 +45,10 @@ class Program
     static void ViewMenu(SocketClient client)
     {
         client.SendMessage("MENU_GET");
+
+        var response = client.RecieveMessage();
+        Console.WriteLine(response);
+
     }
 
     static void SelectMealOptions(SocketClient client)
@@ -51,6 +57,21 @@ class Program
         string classification = Console.ReadLine();
 
         client.SendMessage($"MEAL_SELECT|{classification}");
+
+        var response = client.RecieveMessage();
+        Console.WriteLine(response);
+
+    }
+
+    static void SendNotification(SocketClient client)
+    {
+        Console.WriteLine("Enter Notification: ");
+        string message = Console.ReadLine();
+
+        client.SendMessage($"NOTI_SEND|{message}");
+
+        var response = client.RecieveMessage();
+        Console.WriteLine(response);
     }
 
     static void ExitProgram()
@@ -70,5 +91,15 @@ class Program
 
         string json = JsonConvert.SerializeObject(user);
         client.SendMessage($"LOGIN|{json}");
+
+        var response = client.RecieveMessage();
+        Console.WriteLine(response);
+
+        if (response == "Login Sucessfull")
+        {
+            return;
+        }
+
+        ChefLogin(client);
     }
 }
